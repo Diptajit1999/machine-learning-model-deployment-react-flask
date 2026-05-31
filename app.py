445af6c45c8668afb9d0e4cc6e2795f8cfc45d97
Diptajit Das <diptajitdas66@gmail.com>
2026-05-31 16:59:57 +0530

    Cors support installed

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import pandas as pd
 import pickle
 import json
@@ -6,6 +7,7 @@ import os
 
 app = Flask(__name__)
 
+CORS(app)
 # Load models
 models = {
     "knn": pickle.load(open("models/knn_model.pkl", "rb")),
